459c352b7aabd7792708f923121989fbea06eec7
henryferrari <hcvferrari@gmail.com>
2022-03-02 15:38:24 +0000

    Added #take_off. Planes will not land if airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,11 +3,24 @@ require "plane"
 class Airport
   attr_reader :hangar
 
+  CAPACITY = 20
+
   def initialize
     @hangar = []
   end
 
   def land(plane)
+    fail "Airport full" if full?
     @hangar << plane
+  end
+
+  def take_off(plane)
+    @hangar.delete(plane)
+  end
+
+  private
+
+  def full?
+    hangar.size == CAPACITY
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,18 +2,31 @@ require "airport"
 
 describe Airport do
   let(:airplane) { double :plane }
+  capacity = Airport::CAPACITY
 
   it "should initialize an empty hangar" do
     expect(subject.hangar).to eq([])
   end
 
-  describe "testing the #land method" do
+  describe "#land" do
     it "should respond to #land" do
       expect(subject).to respond_to(:land)
     end
     it "should enter a plane into the hangar" do
       subject.land(airplane)
       expect(subject.hangar).to eq([airplane])
+    end
+    it "should raise error if hangar is full" do
+      capacity.times { subject.land(airplane) }
+      expect { subject.land(airplane) }.to raise_error("Airport full")
+    end
+  end
+
+  describe "#take_off" do
+    it "should remove a plane from the hangar" do
+      subject.land(airplane)
+      subject.take_off(airplane)
+      expect(subject.hangar).to eq([])
     end
   end
 end
